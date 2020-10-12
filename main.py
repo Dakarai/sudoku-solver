9c9122b1fccd4b2fc9857fbb9408a0e95c878caa
Brendan Davy <davybrendan@gmail.com>
2020-10-12 16:13:54 -0700

    Update main.py
    edited algorithm for speed

--- main.py
+++ main.py
@@ -1,59 +1,72 @@
 import numpy
 
 
-#grid = [[0, 0, 5, 0, 0, 1, 3, 9, 0],
-#        [0, 0, 0, 0, 2, 0, 0, 0, 0],
-#        [0, 0, 3, 0, 0, 8, 0, 0, 4],
-#        [0, 0, 0, 0, 0, 7, 4, 0, 6],
-#        [0, 9, 2, 0, 0, 0, 0, 0, 0],
-#        [0, 0, 0, 4, 5, 0, 0, 0, 1],
-#        [1, 0, 0, 0, 0, 0, 0, 0, 0],
-#        [0, 0, 0, 0, 0, 0, 0, 8, 3],
-#        [6, 4, 9, 0, 0, 0, 0, 0, 0]]
-
-grid = [[5, 3, 0, 0, 7, 0, 0, 0, 0],
+grid = [[0, 0, 5, 0, 0, 1, 3, 9, 0],
         [0, 0, 0, 0, 2, 0, 0, 0, 0],
-        [0, 9, 8, 0, 0, 0, 0, 6, 0],
-        [8, 0, 0, 0, 6, 0, 0, 0, 3],
-        [4, 0, 0, 8, 0, 3, 0, 0, 1],
-        [7, 0, 0, 0, 2, 0, 0, 0, 6],
-        [0, 6, 0, 0, 0, 0, 2, 8, 0],
-        [0, 0, 0, 4, 1, 9, 0, 0, 5],
-        [0, 0, 0, 0, 8, 0, 0, 7, 9]]
+        [0, 0, 3, 0, 0, 8, 0, 0, 4],
+        [0, 0, 0, 0, 0, 7, 4, 0, 6],
+        [0, 9, 2, 0, 0, 0, 0, 0, 0],
+        [0, 0, 0, 4, 5, 0, 0, 0, 1],
+        [1, 0, 0, 0, 0, 0, 0, 0, 0],
+        [0, 0, 0, 0, 0, 0, 0, 8, 3],
+        [6, 4, 9, 0, 0, 0, 0, 0, 0]]
+
+#grid = [[5, 3, 0, 0, 7, 0, 0, 0, 0],
+#        [6, 0, 0, 1, 9, 5, 0, 0, 0],
+#        [0, 9, 8, 0, 0, 0, 0, 6, 0],
+#        [8, 0, 0, 0, 6, 0, 0, 0, 3],
+#        [4, 0, 0, 8, 0, 3, 0, 0, 1],
+#        [7, 0, 0, 0, 2, 0, 0, 0, 6],
+#        [0, 6, 0, 0, 0, 0, 2, 8, 0],
+#        [0, 0, 0, 4, 1, 9, 0, 0, 5],
+#        [0, 0, 0, 0, 8, 0, 0, 7, 9]]
 
 
-def is_possible(y, x, n):
-    global grid
+def is_possible(board, y, x, n):
+
     for i in range(0, 9):
-        if grid[y][i] == n:
+        if board[y][i] == n and x != i:
             return False
+
     for i in range(0, 9):
-        if grid[i][x] == n:
+        if grid[i][x] == n and y != i:
             return False
 
-    x0 = (x // 3)
-    y0 = (y // 3)
+    box_x = x // 3
+    box_y = y // 3
 
-    for i in range(y0, y0 + 3):
-        for j in range(x0, x0 + 3):
-            if grid[i][j] == n:
+    for i in range(box_y, box_y + 3):
+        for j in range(box_x, box_x + 3):
+            if grid[i][j] == n and x != j and y != i:
                 return False
     return True
 
 
-def solver():
-    global grid
-    for y in range(9):
-        for x in range(9):
-            if grid[y][x] == 0:
-                for n in range(1, 10):
-                    grid[y][x] = n
-                    solver()
-                    grid[y][x] = 0
+def solver(board):
+    empty_cell = find_empty_cell(board)
+    if not empty_cell:
+        return True
+    else:
+        y, x = empty_cell
 
-                return
+    for i in range(1, 10):
+        if is_possible(board, y, x, i):
+            board[y][x] = i
+
+            if solver(board):
+                return True
+
+            board[y][x] = 0
+
+    return False
 
 
+def find_empty_cell(board):
+    for y in range(0, 9):
+        for x in range(0, 9):
+            if board[y][x] == 0:
+                return (y, x)
 
-solver()
+
+grid = solver(grid)
 print(numpy.array(grid))
