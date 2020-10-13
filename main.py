2438e1f441a214d42930b7d03bd2f2f178afbf09
Brendan Davy <davybrendan@gmail.com>
2020-10-12 19:20:13 -0700

    Update main.py
    cleaned up code

--- main.py
+++ main.py
@@ -18,10 +18,7 @@ def print_grid():
 def possible(row, col, n):
     global grid
     for i in range(9):
-        if grid[row][i] == n:
-            return False
-    for i in range(9):
-        if grid[i][col] == n:
+        if grid[row][i] == n or grid[i][col] == n:
             return False
     square_row = (row // 3) * 3
     square_col = (col // 3) * 3
@@ -42,12 +39,13 @@ def solve():
                     if possible(row, col, digit):
                         grid[row][col] = digit
                         solve()
-                        grid[row][col] == 0
+                        grid[row][col] = 0
                 return
-    printGrid()
+    print("\n Solution: \n")
+    print_grid()
 
 
-puzzle_string = "004300209005009001070060043006002087190007400050083000600000105003508690042910300"
+input_string = "002000000035070000000000098080600040670810000000000000000754000000000069200003080"
 grid = []
-form_grid(puzzle_string)
+form_grid(input_string)
 solve()
