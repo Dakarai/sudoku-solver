a4288d20d1ec1ecd32e5ba6c9750fb9d8dc7ee37
Brendan Davy <davybrendan@gmail.com>
2020-10-12 19:03:53 -0700

    Update main.py

--- main.py
+++ main.py
@@ -1,72 +1,53 @@
-import numpy
+def form_grid(puzzle_string):
+    global grid
+    for i in range(0, len(puzzle_string), 9):
+        row = puzzle_string[i: i + 9]
+        temp = []
+        for block in row:
+            temp.append(int(block))
+        grid.append(temp)
+    print_grid()
 
 
-grid = [[0, 0, 5, 0, 0, 1, 3, 9, 0],
-        [0, 0, 0, 0, 2, 0, 0, 0, 0],
-        [0, 0, 3, 0, 0, 8, 0, 0, 4],
-        [0, 0, 0, 0, 0, 7, 4, 0, 6],
-        [0, 9, 2, 0, 0, 0, 0, 0, 0],
-        [0, 0, 0, 4, 5, 0, 0, 0, 1],
-        [1, 0, 0, 0, 0, 0, 0, 0, 0],
-        [0, 0, 0, 0, 0, 0, 0, 8, 3],
-        [6, 4, 9, 0, 0, 0, 0, 0, 0]]
-
-#grid = [[5, 3, 0, 0, 7, 0, 0, 0, 0],
-#        [6, 0, 0, 1, 9, 5, 0, 0, 0],
-#        [0, 9, 8, 0, 0, 0, 0, 6, 0],
-#        [8, 0, 0, 0, 6, 0, 0, 0, 3],
-#        [4, 0, 0, 8, 0, 3, 0, 0, 1],
-#        [7, 0, 0, 0, 2, 0, 0, 0, 6],
-#        [0, 6, 0, 0, 0, 0, 2, 8, 0],
-#        [0, 0, 0, 4, 1, 9, 0, 0, 5],
-#        [0, 0, 0, 0, 8, 0, 0, 7, 9]]
+def print_grid():
+    global grid
+    for row in grid:
+        print(row)
 
 
-def is_possible(board, y, x, n):
-
-    for i in range(0, 9):
-        if board[y][i] == n and x != i:
+def possible(row, col, n):
+    global grid
+    for i in range(9):
+        if grid[row][i] == n:
             return False
-
-    for i in range(0, 9):
-        if grid[i][x] == n and y != i:
+    for i in range(9):
+        if grid[i][col] == n:
             return False
+    square_row = (row // 3) * 3
+    square_col = (col // 3) * 3
 
-    box_x = x // 3
-    box_y = y // 3
-
-    for i in range(box_y, box_y + 3):
-        for j in range(box_x, box_x + 3):
-            if grid[i][j] == n and x != j and y != i:
+    for i in range(3):
+        for j in range(3):
+            if grid[square_row + i][square_col + j] == n:
                 return False
     return True
 
 
-def solver(board):
-    empty_cell = find_empty_cell(board)
-    if not empty_cell:
-        return True
-    else:
-        y, x = empty_cell
-
-    for i in range(1, 10):
-        if is_possible(board, y, x, i):
-            board[y][x] = i
-
-            if solver(board):
-                return True
-
-            board[y][x] = 0
-
-    return False
+def solve():
+    global grid
+    for row in range(9):
+        for col in range(9):
+            if grid[row][col] == 0:
+                for digit in range(1, 10):
+                    if possible(row, col, digit):
+                        grid[row][col] = digit
+                        solve()
+                        grid[row][col] == 0
+                return
+    printGrid()
 
 
-def find_empty_cell(board):
-    for y in range(0, 9):
-        for x in range(0, 9):
-            if board[y][x] == 0:
-                return (y, x)
-
-
-grid = solver(grid)
-print(numpy.array(grid))
+puzzle_string = "004300209005009001070060043006002087190007400050083000600000105003508690042910300"
+grid = []
+form_grid(puzzle_string)
+solve()
